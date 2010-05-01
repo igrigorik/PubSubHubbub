99c309c07c4d43fcd011e8fe1c31e01cb0d883a0
Ilya Grigorik <ilya@igvita.com>
2010-05-01 19:26:55 -0400

    refactor, code duplication

--- lib/pubsubhubbub/client.rb
+++ lib/pubsubhubbub/client.rb
@@ -19,42 +19,36 @@ module EventMachine
         {'hub.url' => feed, 'hub.mode' => 'publish'}.to_params
       end.join("&")
 
-      r = EventMachine::HttpRequest.new(@hub).post :body => data, :head => HEADERS
-      r.callback { 
-        if r.response_header.status == 204
-          succeed r
-        else
-          fail r
-        end
-      }
-
-      r.errback { fail }
-      r
+      request(:body => data, :head => HEADERS)
     end
 
-     # These command will work only if the callback URL supports confirmation.
+    # These command will work only if the callback URL supports confirmation.
     def subscribe(feed, callback, options = {});   command('subscribe', feed, callback, options);   end
     def unsubscribe(feed, callback, options = {}); command('unsubscribe', feed, callback, options); end
 
     private
-    
-    def command(cmd, feed, callback, options)
-      options['hub.verify'] ||= "sync"
 
-      params = {'hub.topic' => feed, 'hub.mode' => cmd, 'hub.callback' => callback}.merge(options).to_params
-      r = EventMachine::HttpRequest.new(@hub).post :body => params, :head => HEADERS
+      def command(cmd, feed, callback, options)
+        options['hub.verify'] ||= "sync"
+        params = {'hub.topic' => feed, 'hub.mode' => cmd, 'hub.callback' => callback}.merge(options).to_params
 
-      r.callback {
-        if r.response_header.status == 204
-          succeed r
-        else
-          fail r
+        request(:body => params, :head => HEADERS)
+      end
+
+      def request(opts)
+        r = EventMachine::HttpRequest.new(@hub).post opts
+
+        r.callback do
+          if r.response_header.status == 204
+            succeed r
+          else
+            fail r
+          end
         end
-      }
 
-      r.errback { fail }
-      r
-    end
+        r.errback { fail }
+        r
 
+      end
   end
 end
